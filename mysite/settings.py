61446d94a8064db52c5aa05f5ebd48f4554d5c79
Pug <alievadg@mail.ru>
2020-05-15 00:38:54 +0300

    psql added

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'diploma',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
